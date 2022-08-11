82031ef93e34f5177033eb98c048bcc3baf86a69
David Lopes <davidlopes009@gmail.com>
2022-08-11 18:40:26 -0300

    First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+        <h3>Escreva seus interesses</h3>
+        <p>Mini Blog &copy; 2022</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firebase';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0lvtbHFQUS1SYMLIAA-cWSbC3HPvcpqw",
+  authDomain: "miniblog-3f6b3.firebaseapp.com",
+  projectId: "miniblog-3f6b3",
+  storageBucket: "miniblog-3f6b3.appspot.com",
+  messagingSenderId: "15180753581",
+  appId: "1:15180753581:web:fd12b007e08680a72bf424"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {db};
